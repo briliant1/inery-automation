1faaecc3a243b0fecc979e520ec69d3039e32e84
Briliant <mrbriliantone@gmail.com>
2022-11-05 12:05:06 +0700

    update command

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # inery-automation
  
-rm -rf ./inery-automation/; git clone https://github.com/briliant1/inery-automation.git; chmod +x ./inery-automation/command.sh; chmod +x ./inery-automation/main.py; ./inery-automation/command.sh
+rm -rf ./inery-automation/; git clone https://github.com/briliant1/inery-automation.git; chmod +x ./inery-automation/command.sh; chmod +x ./inery-automation/ineryMenu.py; ./inery-automation/command.sh
--- command.sh
+++ command.sh
@@ -1,10 +1,35 @@
 #!/bin/sh
-pwd 
 sudo apt-get update && sudo apt install git && sudo apt install screen
-sudo apt-get install -y make bzip2 automake libbz2-dev libssl-dev doxygen graphviz libgmp3-dev \
-autotools-dev libicu-dev python2.7 python2.7-dev python3 python3-dev python3-pip\
-autoconf libtool curl zlib1g-dev sudo ruby libusb-1.0-0-dev \
-libcurl4-gnutls-dev pkg-config patch llvm-7-dev clang-7 vim-common jq libncurses5 \
+yes | sudo apt-get install make
+yes | sudo apt-get install bzip2
+yes | sudo apt-get install automake
+yes | sudo apt-get install libbz2-dev
+yes | sudo apt-get install libssl-dev
+yes | sudo apt-get install doxygen
+yes | sudo apt-get install graphviz
+yes | sudo apt-get install libgmp3-dev
+yes | sudo apt-get install autotools-dev
+yes | sudo apt-get install libicu-dev
+yes | sudo apt-get install python2.7
+yes | sudo apt-get install python2.7-dev
+yes | sudo apt-get install python3
+yes | sudo apt-get install python3-dev
+yes | sudo apt-get install python3-pip
+yes | sudo apt-get install autoconf
+yes | sudo apt-get install libtool
+yes | sudo apt-get install curl
+yes | sudo apt-get install zlib1g-dev
+yes | sudo apt-get install sudo
+yes | sudo apt-get install ruby
+yes | sudo apt-get install libusb-1.0-0-dev
+yes | sudo apt-get install libcurl4-gnutls-dev
+yes | sudo apt-get install pkg-config
+yes | sudo apt-get install patch
+yes | sudo apt-get install llvm-7-dev
+yes | sudo apt-get install clang-7
+yes | sudo apt-get install vim-common
+yes | sudo apt-get install jq
+yes | sudo apt-get install libncurses5
 sudo ufw allow 22
 sudo ufw allow 8888
 sudo ufw allow 9010
